cc657625b4cd929638aa86c82143630484a2c021
ruritoBlogger <ruriro0125@gmail.com>
2021-11-28 19:40:04 +0900

    chore: lint files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
-import styles from "./index.module.sass";
+import styles from './index.module.sass'
 
 const Home: React.FC = () => {
-  return <div className={styles.className}>hello world!</div>;
-};
+  return <div className={styles.className}>hello world!</div>
+}
 
-export default Home;
+export default Home
