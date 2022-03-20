9f9f4fbcc21828bb2c1b1788886659cc6a1116c3
ruritoBlogger <ruriro0125@gmail.com>
2022-03-20 16:35:00 +0900

    fix: fix company domain with toko api

--- src/domains/Company.ts
+++ src/domains/Company.ts
@@ -3,10 +3,10 @@ import * as t from 'io-ts'
 export const CompanyCodec = t.type({
   name: t.string,
   id: t.number,
-  IDIndustry: t.number,
-  IdentificationCode: t.number,
-  CreatedAt: t.number,
-  UpdatedAt: t.number,
+  industryID: t.number,
+  identificationCode: t.number,
+  createdAt: t.string,
+  updatedAt: t.string,
 })
 
 export type Company = t.TypeOf<typeof CompanyCodec>
