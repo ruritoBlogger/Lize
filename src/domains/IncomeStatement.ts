db2f01b0e808f533725e1da8205241efd3408546
ruritoblogger <rurito.blog@gmail.com>
2022-03-21 15:25:09 +0900

    fix: fix domains with toko api

--- src/domains/IncomeStatement.ts
+++ src/domains/IncomeStatement.ts
@@ -1,14 +1,14 @@
 import * as t from 'io-ts'
 
 export const IncomeStatementCodec = t.type({
-  IDfinancial: t.number,
+  financialID: t.number,
   id: t.number,
-  TotalSales: t.number,
-  OperatingIncome: t.number,
-  OrdinaryIncome: t.number,
-  NetIncome: t.number,
-  CreatedAt: t.number,
-  UpdatedAt: t.number,
+  totalSales: t.number,
+  operatingIncome: t.number,
+  ordinaryIncome: t.number,
+  netIncome: t.number,
+  createdAt: t.number,
+  updatedAt: t.number,
 })
 
 export type IncomeStatement = t.TypeOf<typeof IncomeStatementCodec>
--- src/domains/Indextype.ts
+++ src/domains/Indextype.ts
@@ -1,21 +1,15 @@
 import * as t from 'io-ts'
 
 export const IndexCodec = t.type({
-  IDfinancial: t.number,
+  financialID: t.number,
   id: t.number,
-  CapitalAdequacyRatio: t.number,
-  ROE: t.number,
-  ROA: t.number,
-  PER: t.number,
-  PBR: t.number,
-  EPS: t.number,
-  PCFR: t.number,
-  YieldGap: t.number,
-  EBITDA: t.number,
-  EV: t.number,
-  EVdivEBITDA: t.number,
-  CreatedAt: t.number,
-  UpdatedAt: t.number,
+  roe: t.number,
+  roa: t.number,
+  per: t.number,
+  pbr: t.number,
+  eps: t.number,
+  createdAt: t.number,
+  updatedAt: t.number,
 })
 
 export type Index = t.TypeOf<typeof IndexCodec>
