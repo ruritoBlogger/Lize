cce62a6ae0b74e33f4a6055eb3ea98191eda2dba
ruritoBlogger <ruriro0125@gmail.com>
2022-03-21 17:08:57 +0900

    feat: reduxの設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,20 @@ import './_app.sass'
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Provider } from 'react-redux'
+
+import { store } from '../features'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>タイトル</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <>
+        <Head>
+          <title>タイトル</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    </Provider>
   )
 }
 
