48c39a2093d86ddbb8ed2f3fd958df4a23cc602b
ruritoBlogger <rurito.blog@gmail.com>
2022-03-26 15:54:14 +0900

    feat: 株価情報をreduxで管理するようにした

--- src/features/rootReducer.ts
+++ src/features/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import { companySlice } from './company'
 import { industrySlice } from './industry'
+import { stockPriceSlice } from './stockPrice'
 
 export const rootReducer = combineReducers({
   industry: industrySlice.reducer,
   company: companySlice.reducer,
+  stockPrice: stockPriceSlice.reducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
