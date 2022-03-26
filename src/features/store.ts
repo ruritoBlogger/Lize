a846ba83d284f10a910ac3b7a10ec9607a0dac28
ruritoBlogger <ruriro0125@gmail.com>
2022-03-26 13:25:38 +0900

    feat: useSelectorする際に型補完が動くようにした

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,6 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import { rootReducer } from './rootReducer'
+import { rootReducer, RootState } from './rootReducer'
+
+/**
+ * NOTE:
+ * react-reduxで提供されているDefaultRootStateはdefaultだとstateに対して型推論が働かない
+ * なので型推論を働かせるためにいくつかの要素を上書きする
+ *
+ * URL: https://qiita.com/Takepepe/items/6addcb1b0facb8c6ff1f
+ */
+// TODO: useDispatch, useStoreの型推論も出来るようにする
+declare module 'react-redux' {
+  // eslint-disable-next-line
+  interface DefaultRootState extends RootState {}
+}
 
 export const store = configureStore({
   reducer: rootReducer,
