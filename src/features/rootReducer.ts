325a376fa69f8b052da0472002525a644a333cdd
ruritoBlogger <rurito.blog@gmail.com>
2022-03-26 16:01:37 +0900

    feat: 財務諸表データをreduxで管理するようにした

--- src/features/rootReducer.ts
+++ src/features/rootReducer.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
 import { companySlice } from './company'
+import { financialStatementSlice } from './financialStatement'
 import { industrySlice } from './industry'
 import { stockPriceSlice } from './stockPrice'
 
@@ -8,6 +9,7 @@ export const rootReducer = combineReducers({
   industry: industrySlice.reducer,
   company: companySlice.reducer,
   stockPrice: stockPriceSlice.reducer,
+  financialStatement: financialStatementSlice.reducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
